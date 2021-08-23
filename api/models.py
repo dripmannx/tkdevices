78032f9972ae1eff12c05eaa229118809d038aa6
Luca Düpmann <luca.duepmann@gmail.com>
2021-08-23 10:59:38 +0200

    changed column text alignments

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.fields import NullBooleanField
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.fields import URLField
 # Create your models here.
 class Device(models.Model):
     serialnumber = models.CharField(max_length=12, unique=True, blank=False)
@@ -12,4 +13,6 @@ class Device(models.Model):
     removed_from_DEP = models.BooleanField(default=False)
     status_defect = models.BooleanField(default=False)
 
+class Handout(models.Model):
+    link = URLField()
 
